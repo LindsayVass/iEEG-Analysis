fe40e8ba3905dfe71cb7d3f5482b4731d82b101e
LindsayVass <lindsay.k.morgan@gmail.com>
2015-06-02 12:15:15 -0700

    updated subject structure so it's easier to interpret; also added numEDFs

--- Group Analysis/Subject Info/MakeSessionInfoStruct.m
+++ Group Analysis/Subject Info/MakeSessionInfoStruct.m
@@ -4,14 +4,29 @@
 % 29 May 2015
 
 
-field1 = 'subjectID';     value1 = {'UCDMC13','UCDMC14','UCDMC15'};
-field2 = 'teleporter';    value2 = {{'TeleporterA'}, ...
-                                    {'TeleporterA', 'TeleporterB'}, ...
-                                    {'TeleporterA', 'TeleporterB'}};
-field3 = 'chanList';      value3 = {{'LAD1' 'LHD1'}, ...
-                                    {'LAD1' 'LHD1' 'LHD2' 'RAD1' 'RHD1' 'RHD2'}, ...
-                                    {'RAD3' 'RAD4' 'RAD5' 'RAD6' 'RHD1' 'RHD2' 'RHD3' 'RHD4' 'LAD3' 'LAD4' 'LAD5' 'LHD1' 'LHD2' 'LHD3'}};
+field1 = 'subjectID';
+field2 = 'teleporter';
+field3 = 'numEDFs';
+field4 = 'chanList'; 
+sessionInfo = struct(field1, {}, field2, {}, field3, {}, field4, {});
 
-sessionInfo = struct(field1, value1, field2, value2, field3, value3);
+
+% UCDMC13
+sessionInfo(1).subjectID  = 'UCDMC13';
+sessionInfo(1).teleporter = 'TeleporterA';
+sessionInfo(1).numEDFs    = '1';
+sessionInfo(1).chanList   = {'LAD1' 'LHD1'};
+
+% UCDMC14
+sessionInfo(2).subjectID  = 'UCDMC14';
+sessionInfo(2).teleporter = {'TeleporterA', 'TeleporterB'};
+sessionInfo(2).numEDFs    = {'1', '1'};
+sessionInfo(2).chanList   = {'LAD1' 'LHD1' 'LHD2' 'RAD1' 'RHD1' 'RHD2'};
+
+% UCDMC15
+sessionInfo(3).subjectID  = 'UCDMC15';
+sessionInfo(3).teleporter = {'TeleporterA', 'TeleporterB'};
+sessionInfo(3).numEDFs    = {'2', '2'};
+sessionInfo(3).chanList   = {'RAD3' 'RAD4' 'RAD5' 'RAD6' 'RHD1' 'RHD2' 'RHD3' 'RHD4' 'LAD3' 'LAD4' 'LAD5' 'LHD1' 'LHD2' 'LHD3'};
 
 save('/Users/Lindsay/Documents/MATLAB/iEEG/Group Analysis/Subject Info/SessionInfo.mat', 'sessionInfo');
