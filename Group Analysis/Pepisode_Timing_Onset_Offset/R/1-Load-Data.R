66e82eaa4876e10645e1613af77c28e440c17aa5
LindsayVass <lindsay.k.morgan@gmail.com>
2015-06-22 13:08:45 -0700

    accidentally overwrote the original file; restoring it

--- Group Analysis/Pepisode_Timing_Onset_Offset/R/1-Load-Data.R
+++ Group Analysis/Pepisode_Timing_Onset_Offset/R/1-Load-Data.R
@@ -1,52 +1,45 @@
 # Script Name:  1-Load-Data.R
 # Author:       Lindsay Vass
-# Date:         5 June 2015
+# Date:         3 June 2015
 # Purpose:      This script will load the csv files output by the Matlab
-#               function "Run_Teleporter_Pepisode_Timing_Onset_Offset.m"
+#               function "Run_Teleporter_Epoched_Power.m"
+
+
+# Functions ---------------------------------------------------------------
+
+makeSessionInfo <- function() {
+  
+  UCDMC13 <- data.frame(Subject = "UCDMC13", Session = "TeleporterA")
+  UCDMC14 <- data.frame(Subject = "UCDMC14", Session = c("TeleporterA", "TeleporterB"))
+  UCDMC15 <- data.frame(Subject = "UCDMC15", Session = c("TeleporterA", "TeleporterB"))
+  
+  sessionInfo <- rbind(UCDMC13, UCDMC14, UCDMC15)
+  
+  return(sessionInfo)
+  
+}
+
+loadSubjectData <- function(subject, session, csvFileStem) {
+  
+  inputFile <- paste0('csv/', subject, '_', session, '_', csvFileStem)
+  tempData  <- read.csv(inputFile, header = TRUE)
+  return(tempData)
+  
+}
 
 
 # Script ------------------------------------------------------------------
 
-# data is stored in three csv files:
-# observation_characteristics
-# NT_data
-# FT_data
+# Make a data frame that contains which subjects and sessions we have data for
+sessionInfo <- makeSessionInfo()
 
-charFiles <- dir('csv/', pattern = "*characteristics.csv")
-ntFiles <- dir('csv/', pattern = "*NT_data.csv")
-ftFiles <- dir('csv/', pattern = "*FT_data.csv")
-
-charData <- data.frame()
-ntData   <- data.frame()
-ftData   <- data.frame()
-
-for (thisFile in 1:length(charFiles)) {
+# Load the data from each subject's session(s) and combine into one data frame
+allData <- data.frame()
+for (thisRow in 1:nrow(sessionInfo)) {
   
-  tempData <- read.csv(paste0('csv/', charFiles[thisFile]), header = TRUE)
-  charData <- rbind(charData, tempData)
+  subjectData <- loadSubjectData(sessionInfo$Subject[thisRow], 
+                                 sessionInfo$Session[thisRow],
+                                 'epoched_power.csv')
   
-  # add information about subject, session, and electrode to data frame based on
-  # file name
-  fileNameParts <- strsplit(ntFiles[thisFile], split = "_")
-  
-  tempData <- read.csv(paste0('csv/', ntFiles[thisFile]), header = TRUE)
-  tempData$Subject   <- fileNameParts[[1]][1]
-  tempData$Session   <- fileNameParts[[1]][2]
-  tempData$Electrode <- fileNameParts[[1]][5]
-  
-  ntData   <- rbind(ntData, tempData)
-  
-  tempData <- read.csv(paste0('csv/', ftFiles[thisFile]), header = TRUE)
-  tempData$Subject   <- fileNameParts[[1]][1]
-  tempData$Session   <- fileNameParts[[1]][2]
-  tempData$Electrode <- fileNameParts[[1]][5]
-  
-  ftData   <- rbind(ftData, tempData)
-  
+  allData <- rbind(allData, subjectData)
 }
-
-remove(tempData)
-
-# save concatenated data
-dir.create('Rda')
-save(file = 'Rda/allRawData.Rda', list = c('charData', 'ntData', 'ftData'))
