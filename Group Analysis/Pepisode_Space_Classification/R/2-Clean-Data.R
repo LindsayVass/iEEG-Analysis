8adae8dd9fd8235cd7ac1fb37c9d38f7cbef7f5e
LindsayVass <lindsay.k.morgan@gmail.com>
2015-06-23 10:18:35 -0700

    scripts to test whether pepisode can be used to classify the spatial condition

---
+++ Group Analysis/Pepisode_Space_Classification/R/2-Clean-Data.R
@@ -0,0 +1,128 @@
+# Script Name:  2-Clean-Data.R
+# Author:       Lindsay Vass
+# Date:         22 June 2015
+# Purpose:      This script will clean the data frames produced by "1-Load-Data.R"
+
+library(dplyr)
+load('Rda/allRawData.Rda')
+
+
+# Functions ---------------------------------------------------------------
+
+cutVariablesByTime <- function(varNames, cutPoints, binNames) {
+  
+  # R added X to positive timepoints and X. to negative timepoints so remove
+  # them before converting to numeric
+  origVarNames <- varNames
+  varNames <- sub("^X\\.", "-", varNames)
+  varNames <- sub("^X", "", varNames)
+  varNames <- as.numeric(varNames)
+  
+  # Cut into time bins
+  varNames <- cut(varNames, cutPoints, binNames)
+  
+  # Return a data frame that contains the original variable names and their
+  # respective time bins
+  df <- data.frame(Names = origVarNames, Bins = varNames, stringsAsFactors = FALSE)
+  
+}
+
+getMeanPepisodeWithinBin <- function(varNames, binName, pepisodeData) {
+  
+  # get the column numbers corresponding to this binName
+  thisBinNames <- varNames %>%
+    filter(Bins == binName) %>%
+    select(Names) 
+  colNums <- match(thisBinNames$Names, names(pepisodeData))
+  
+  # take the mean across the columns indicated by colNums
+  tempData <- pepisodeData %>%
+    select(colNums) %>%
+    rowMeans()
+  
+  # combine the mean pepisode data with the info in the original data frame
+  output <- pepisodeData %>%
+    select(ObservationID) %>%
+    mutate(Pepisode = tempData,
+           TimeBin = binName) 
+  
+}
+
+# Clean charData ----------------------------------------------------------
+
+# make new variable electrodeID to capture the fact that we treat the same
+# electrode in different sessions as different observations
+cleanCharData <- charData %>%
+  select(ObservationID:TrialTimeType, Frequency) %>%
+  mutate(ElectrodeID = paste(SubjectID, Teleporter, Electrode, sep = "_")) %>%
+  select(-c(SubjectID, Teleporter, Electrode)) %>%
+  mutate(ElectrodeID = factor(ElectrodeID))
+
+# make ObservationID unique across subjects
+cleanCharData <- cleanCharData %>%
+  mutate(ObservationID = paste(ElectrodeID, ObservationID, sep = "_"))
+
+# Cut up the frequencies into bands
+frequencies    <- unique(cleanCharData$Frequency)
+freqBandBreaks <- c(0, 4, 8, 12, 30, 182)
+freqBandNames  <- c("Delta", "Theta", "Alpha", "Beta", "Gamma")
+cleanCharData <- cleanCharData %>%
+  mutate(FrequencyBand = cut(Frequency, freqBandBreaks, labels = freqBandNames)) %>%
+  select(-Frequency)
+
+# Divide time points into pre/tele/post bins
+timeBinBreaksNt <- c(-1830, 0, 1831, 3661)
+timeBinBreaksFt <- c(-2830, 0, 2831, 5661)
+timeBinNames  <- c("Pre", "Tele", "Post")
+
+ntNames <- ntData %>%
+  select(starts_with("X")) %>%
+  names() %>%
+  cutVariablesByTime(timeBinBreaksNt, timeBinNames)
+ftNames <- ftData %>%
+  select(starts_with("X")) %>%
+  names() %>%
+  cutVariablesByTime(timeBinBreaksFt, timeBinNames)
+
+# Keep only episode data and make the ObservationID variable
+tempNtData <- ntData %>%
+  filter(ObservationType == "Episode") %>%
+  mutate(ObservationID = paste(Subject, Session, Electrode, ObservationID, sep = "_")) 
+tempFtData <- ftData %>%
+  filter(ObservationType == "Episode") %>%
+  mutate(ObservationID = paste(Subject, Session, Electrode, ObservationID, sep = "_"))
+
+# Get the mean pepisode for each time bin
+cleanNtData <- data.frame(ObservationID = NA, Pepisode = NA, TimeBin = NA)
+for (thisBin in 1:nlevels(ntNames$Bins)) {
+  
+  tempData <- getMeanPepisodeWithinBin(ntNames, levels(ntNames$Bins)[thisBin], tempNtData)
+  cleanNtData <- rbind(cleanNtData, tempData)
+  
+}
+
+cleanFtData <- data.frame(ObservationID = NA, Pepisode = NA, TimeBin = NA)
+for (thisBin in 1:nlevels(ftNames$Bins)) {
+  
+  tempData <- getMeanPepisodeWithinBin(ftNames, levels(ftNames$Bins)[thisBin], tempFtData)
+  cleanFtData <- rbind(cleanFtData, tempData)
+  
+}
+
+# Join the data together
+cleanNtData <- inner_join(cleanCharData, cleanNtData)
+cleanFtData <- inner_join(cleanCharData, cleanFtData)
+cleanData <- rbind(cleanNtData, cleanFtData)
+
+# Put our conditions in order
+timeOrder  <- c('NT','FT')
+spaceOrder <- c('NS', 'FS')
+timePointOrder <- c('Pre', 'Tele', 'Post')
+cleanData$TrialTimeType  <- factor(cleanData$TrialTimeType, levels = timeOrder)
+cleanData$TrialSpaceType <- factor(cleanData$TrialSpaceType, levels = spaceOrder)
+cleanData$TimeBin <- factor(cleanData$TimeBin, levels = timePointOrder)
+
+
+# Save
+save(file = 'Rda/allCleanData.Rda', list = 'cleanData')
+
---
+++ Group Analysis/Pepisode_Space_Classification/R/3-Analyze-Data.R
@@ -0,0 +1,158 @@
+# Script Name:  3-Analyze-Data.R
+# Author:       Lindsay Vass
+# Date:         22 June 2015
+# Purpose:      This script will analyze the data output by 2-Clean-Data.R. It
+#               will test whether pepisode during the teleporter can be used to
+#               classify the teleportation event based on its space type (NS/FS).
+
+library(dplyr)
+library(kernlab)
+
+load('Rda/allCleanData.Rda')
+
+
+# Functions ---------------------------------------------------------------
+
+runClassifier <- function(trainingData, testingData) {
+  
+  tryCatch(fit <- ksvm(TrialSpaceType ~ ., data = trainingData),
+           error = function(e) {
+             fit <- ksvm(TrialSpaceType ~ ., data = trainingData)
+           })
+  
+  predictions <- predict(fit, testingData[, 2], type = "response")
+  confusionMatrix <- table(predictions, testingData$TrialSpaceType)
+  accuracy <- sum(diag(confusionMatrix)) / sum(confusionMatrix)
+  
+}
+
+
+# Perform classification --------------------------------------------------
+
+classificationResults <- data.frame()
+
+for (thisElectrode in 7:nlevels(cleanData$ElectrodeID)) {
+  
+  cat('\n\n-------------------------------------------------\n\n')
+  cat(levels(cleanData$ElectrodeID)[thisElectrode])
+  
+  for (thisFreqBand in 1:nlevels(cleanData$FrequencyBand)) {
+    
+    cat('\n')
+    cat(levels(cleanData$FrequencyBand)[thisFreqBand])
+    cat('\n')
+    
+    thisData <- cleanData %>%
+      filter(TimeBin == "Tele" &
+               ElectrodeID == levels(ElectrodeID)[thisElectrode] &
+               FrequencyBand == levels(FrequencyBand)[thisFreqBand])
+    
+    numObservations <- thisData %>%
+      group_by(TrialSpaceType) %>%
+      summarise(Count = n())
+    
+    numTimeObservations <- thisData %>%
+      group_by(TrialTimeType, TrialSpaceType) %>%
+      summarise(Count = n())
+    
+    # percentage of the data set to use to train the classifier
+    trainingPercent <- 0.75 
+    trainingSize    <- floor(trainingPercent * min(numObservations$Count))
+    testingSize     <- min(numObservations$Count) - trainingSize
+    trainingTimeSize <- floor(trainingPercent * min(numTimeObservations$Count))
+    testingTimeSize  <- min(numTimeObservations$Count) - trainingTimeSize
+    
+    numIterations <- 1000
+    
+    
+    for (thisIteration in 1:numIterations) {
+      
+      #### Run classification using both NT and FT ####
+      
+      # first sample so that both conditions have the same number of observations
+      equalSizeData <- thisData %>%
+        group_by(TrialSpaceType) %>%
+        sample_n(min(numObservations$Count))
+      
+      # make training and testing sets
+      trainingData <- equalSizeData %>%
+        sample_n(trainingSize) 
+      testingData  <- suppressMessages(anti_join(equalSizeData, trainingData))
+      
+      # remove unneeded columns
+      trainingData <- trainingData %>%
+        select(TrialSpaceType, Pepisode)
+      testingData  <- testingData %>%
+        select(TrialSpaceType, Pepisode)
+      
+      # run classifier
+      accuracy <- runClassifier(trainingData, testingData)
+      
+      thisResult <- data.frame(ElectrodeID = levels(cleanData$ElectrodeID)[thisElectrode],
+                               FrequencyBand = levels(cleanData$FrequencyBand)[thisFreqBand],
+                               Model = "Both",
+                               Iteration = thisIteration,
+                               Accuracy = accuracy)
+      classificationResults <- rbind(classificationResults, thisResult)
+      
+      #### Run classification using NT only ####
+      
+      # first sample so that both conditions have the same number of observations
+      equalSizeData <- thisData %>%
+        filter(TrialTimeType == "NT") %>%
+        group_by(TrialSpaceType) %>%
+        sample_n(min(numTimeObservations$Count))
+      
+      # make training and testing sets
+      trainingData <- equalSizeData %>%
+        sample_n(trainingTimeSize) 
+      testingData  <- suppressMessages(anti_join(equalSizeData, trainingData))
+      
+      # remove unneeded columns
+      trainingData <- trainingData %>%
+        select(TrialSpaceType, Pepisode)
+      testingData  <- testingData %>%
+        select(TrialSpaceType, Pepisode)
+      
+      # run classifier
+      accuracy <- runClassifier(trainingData, testingData)
+      
+      thisResult <- data.frame(ElectrodeID = levels(cleanData$ElectrodeID)[thisElectrode],
+                               FrequencyBand = levels(cleanData$FrequencyBand)[thisFreqBand],
+                               Model = "NT",
+                               Iteration = thisIteration,
+                               Accuracy = accuracy)
+      classificationResults <- rbind(classificationResults, thisResult)
+      
+      #### Run classification using FT only ####
+      
+      # first sample so that both conditions have the same number of observations
+      equalSizeData <- thisData %>%
+        filter(TrialTimeType == "FT") %>%
+        group_by(TrialSpaceType) %>%
+        sample_n(min(numTimeObservations$Count))
+      
+      # make training and testing sets
+      trainingData <- equalSizeData %>%
+        sample_n(trainingTimeSize) 
+      testingData  <- suppressMessages(anti_join(equalSizeData, trainingData))
+      
+      # remove unneeded columns
+      trainingData <- trainingData %>%
+        select(TrialSpaceType, Pepisode)
+      testingData  <- testingData %>%
+        select(TrialSpaceType, Pepisode)
+      
+      # run classifier
+      accuracy <- runClassifier(trainingData, testingData)
+      
+      thisResult <- data.frame(ElectrodeID = levels(cleanData$ElectrodeID)[thisElectrode],
+                               FrequencyBand = levels(cleanData$FrequencyBand)[thisFreqBand],
+                               Model = "FT",
+                               Iteration = thisIteration,
+                               Accuracy = accuracy)
+      classificationResults <- rbind(classificationResults, thisResult)
+      
+    }
+  }
+}
