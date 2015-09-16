0cac2a2c02976209990dbd1a7f3ab850f7c7fa53
LindsayVass <lindsay.k.morgan@gmail.com>
2015-09-16 13:43:13 -0700

    create structure containing depth electrodes, good channels, and EEG paths

---
+++ Group Analysis/Subject Info/MakeSessionInfoStruct2.m
@@ -0,0 +1,98 @@
+% Build data structure for analysis
+%
+% Lindsay Vass
+% 16 September 2015
+
+
+% UCDMC13
+sessionInfo(1).subjectID = {'UCDMC13'};
+
+sessionInfo(1).teleporter(1).name = {'TeleporterA'};
+
+sessionInfo(1).teleporter(1).depths(1).name           = {'LAD'};
+sessionInfo(1).teleporter(1).depths(1).chanList       = {'LAD1'};
+sessionInfo(1).teleporter(1).depths(1).epochedEEGPath = {'/Users/Lindsay/Documents/MATLAB/iEEG/Subjects/UCDMC13/Epoched Data/UCDMC13_TeleporterA_epoched_LAD_noSpikes_noWaves.set'};
+
+sessionInfo(1).teleporter(1).depths(2).name           = {'LHD'};
+sessionInfo(1).teleporter(1).depths(2).chanList       = {'LHD1'};
+sessionInfo(1).teleporter(1).depths(2).epochedEEGPath = {'/Users/Lindsay/Documents/MATLAB/iEEG/Subjects/UCDMC13/Epoched Data/UCDMC13_TeleporterA_epoched_LHD_noSpikes_noWaves.set'};
+
+% UCDMC14
+sessionInfo(2).subjectID = {'UCDMC14'};
+
+sessionInfo(2).teleporter(1).name = {'TeleporterA'};
+
+sessionInfo(2).teleporter(1).depths(1).name           = {'LAD'};
+sessionInfo(2).teleporter(1).depths(1).chanList       = {'LAD1'};
+sessionInfo(2).teleporter(1).depths(1).epochedEEGPath = {'/Users/Lindsay/Documents/MATLAB/iEEG/Subjects/UCDMC14/Epoched Data/UCDMC14_TeleporterA_epoched_LAD_noSpikes_noWaves.set'};
+
+sessionInfo(2).teleporter(1).depths(2).name           = {'LHD'};
+sessionInfo(2).teleporter(1).depths(2).chanList       = {'LHD1', 'LHD2'};
+sessionInfo(2).teleporter(1).depths(2).epochedEEGPath = {'/Users/Lindsay/Documents/MATLAB/iEEG/Subjects/UCDMC14/Epoched Data/UCDMC14_TeleporterA_epoched_LHD_noSpikes_noWaves.set'};
+
+sessionInfo(2).teleporter(1).depths(3).name           = {'RAD'};
+sessionInfo(2).teleporter(1).depths(3).chanList       = {'RAD1'};
+sessionInfo(2).teleporter(1).depths(3).epochedEEGPath = {'/Users/Lindsay/Documents/MATLAB/iEEG/Subjects/UCDMC14/Epoched Data/UCDMC14_TeleporterA_epoched_RAD_noSpikes_noWaves.set'};
+
+sessionInfo(2).teleporter(1).depths(4).name           = {'RHD'};
+sessionInfo(2).teleporter(1).depths(4).chanList       = {'RHD1', 'RHD2'};
+sessionInfo(2).teleporter(1).depths(4).epochedEEGPath = {'/Users/Lindsay/Documents/MATLAB/iEEG/Subjects/UCDMC14/Epoched Data/UCDMC14_TeleporterA_epoched_RHD_noSpikes_noWaves.set'};
+
+sessionInfo(2).teleporter(2).name = {'TeleporterB'};
+
+sessionInfo(2).teleporter(2).depths(1).name           = {'LAD'};
+sessionInfo(2).teleporter(2).depths(1).chanList       = {'LAD1'};
+sessionInfo(2).teleporter(2).depths(1).epochedEEGPath = {'/Users/Lindsay/Documents/MATLAB/iEEG/Subjects/UCDMC14/Epoched Data/UCDMC14_TeleporterB_epoched_LAD_noSpikes_noWaves.set'};
+
+sessionInfo(2).teleporter(2).depths(2).name           = {'LHD'};
+sessionInfo(2).teleporter(2).depths(2).chanList       = {'LHD1', 'LHD2'};
+sessionInfo(2).teleporter(2).depths(2).epochedEEGPath = {'/Users/Lindsay/Documents/MATLAB/iEEG/Subjects/UCDMC14/Epoched Data/UCDMC14_TeleporterB_epoched_LHD_noSpikes_noWaves.set'};
+
+sessionInfo(2).teleporter(2).depths(3).name           = {'RAD'};
+sessionInfo(2).teleporter(2).depths(3).chanList       = {'RAD1'};
+sessionInfo(2).teleporter(2).depths(3).epochedEEGPath = {'/Users/Lindsay/Documents/MATLAB/iEEG/Subjects/UCDMC14/Epoched Data/UCDMC14_TeleporterB_epoched_RAD_noSpikes_noWaves.set'};
+
+sessionInfo(2).teleporter(2).depths(4).name           = {'RHD'};
+sessionInfo(2).teleporter(2).depths(4).chanList       = {'RHD1', 'RHD2'};
+sessionInfo(2).teleporter(2).depths(4).epochedEEGPath = {'/Users/Lindsay/Documents/MATLAB/iEEG/Subjects/UCDMC14/Epoched Data/UCDMC14_TeleporterB_epoched_RHD_noSpikes_noWaves.set'};
+
+% UCDMC15
+sessionInfo(3).subjectID = {'UCDMC15'};
+
+sessionInfo(3).teleporter(1).name = {'TeleporterA'};
+
+sessionInfo(3).teleporter(1).depths(1).name           = {'RAD'};
+sessionInfo(3).teleporter(1).depths(1).chanList       = {'RAD3', 'RAD4', 'RAD5', 'RAD6'};
+sessionInfo(3).teleporter(1).depths(1).epochedEEGPath = {'/Users/Lindsay/Documents/MATLAB/iEEG/Subjects/UCDMC15/Epoched Data/UCDMC15_TeleporterA_epoched_RAD_noSpikes_noWaves.set'};
+
+sessionInfo(3).teleporter(1).depths(2).name           = {'RHD'};
+sessionInfo(3).teleporter(1).depths(2).chanList       = {'RHD1', 'RHD2', 'RHD3', 'RHD4'};
+sessionInfo(3).teleporter(1).depths(2).epochedEEGPath = {'/Users/Lindsay/Documents/MATLAB/iEEG/Subjects/UCDMC15/Epoched Data/UCDMC15_TeleporterA_epoched_RHD_noSpikes_noWaves.set'};
+
+sessionInfo(3).teleporter(1).depths(3).name           = {'LAD'};
+sessionInfo(3).teleporter(1).depths(3).chanList       = {'LAD3', 'LAD4', 'LAD5'};
+sessionInfo(3).teleporter(1).depths(3).epochedEEGPath = {'/Users/Lindsay/Documents/MATLAB/iEEG/Subjects/UCDMC15/Epoched Data/UCDMC15_TeleporterA_epoched_LAD_noSpikes_noWaves.set'};
+
+sessionInfo(3).teleporter(1).depths(4).name           = {'LHD'};
+sessionInfo(3).teleporter(1).depths(4).chanList       = {'LHD1', 'LHD2', 'LHD3'};
+sessionInfo(3).teleporter(1).depths(4).epochedEEGPath = {'/Users/Lindsay/Documents/MATLAB/iEEG/Subjects/UCDMC15/Epoched Data/UCDMC15_TeleporterA_epoched_LHD_noSpikes_noWaves.set'};
+
+sessionInfo(3).teleporter(2).name = {'TeleporterB'};
+
+sessionInfo(3).teleporter(2).depths(1).name           = {'RAD'};
+sessionInfo(3).teleporter(2).depths(1).chanList       = {'RAD3', 'RAD4', 'RAD5', 'RAD6'};
+sessionInfo(3).teleporter(2).depths(1).epochedEEGPath = {'/Users/Lindsay/Documents/MATLAB/iEEG/Subjects/UCDMC15/Epoched Data/UCDMC15_TeleporterB_epoched_RAD_noSpikes_noWaves.set'};
+
+sessionInfo(3).teleporter(2).depths(2).name           = {'RHD'};
+sessionInfo(3).teleporter(2).depths(2).chanList       = {'RHD1', 'RHD2', 'RHD3', 'RHD4'};
+sessionInfo(3).teleporter(2).depths(2).epochedEEGPath = {'/Users/Lindsay/Documents/MATLAB/iEEG/Subjects/UCDMC15/Epoched Data/UCDMC15_TeleporterB_epoched_RHD_noSpikes_noWaves.set'};
+
+sessionInfo(3).teleporter(2).depths(3).name           = {'LAD'};
+sessionInfo(3).teleporter(2).depths(3).chanList       = {'LAD3', 'LAD4', 'LAD5'};
+sessionInfo(3).teleporter(2).depths(3).epochedEEGPath = {'/Users/Lindsay/Documents/MATLAB/iEEG/Subjects/UCDMC15/Epoched Data/UCDMC15_TeleporterB_epoched_LAD_noSpikes_noWaves.set'};
+
+sessionInfo(3).teleporter(2).depths(4).name           = {'LHD'};
+sessionInfo(3).teleporter(2).depths(4).chanList       = {'LHD1', 'LHD2', 'LHD3'};
+sessionInfo(3).teleporter(2).depths(4).epochedEEGPath = {'/Users/Lindsay/Documents/MATLAB/iEEG/Subjects/UCDMC15/Epoched Data/UCDMC15_TeleporterB_epoched_LHD_noSpikes_noWaves.set'};
+
+save('/Users/Lindsay/Documents/MATLAB/iEEG/Group Analysis/Subject Info/SessionInfo2.mat', 'sessionInfo');
