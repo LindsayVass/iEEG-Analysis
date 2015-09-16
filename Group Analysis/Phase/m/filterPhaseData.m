f3bfc71b026b65db659191880d08ef45b9bf711d
LindsayVass <lindsay.k.morgan@gmail.com>
2015-09-16 10:45:48 -0700

    functions to run phase locking analysis

---
+++ Group Analysis/Phase/m/filterPhaseData.m
@@ -0,0 +1,34 @@
+function [phaseDataSample, goodFreq, badFreq] = filterPhaseData(phaseData, timeInterval, eegTimes, frequencies, samplingRate, numCycles)
+% filterPhaseData: Take a dataset of phases and filter data, keeping only
+% values within the specified time interval, and at frequencies that exceed
+% the minimum determined by numCycles (must have at least numCycles within
+% the time interval)
+% >> [phaseDataSample, goodFreq, badFreq] = filterPhaseData(phaseData, timeInterval, eegTimes, frequencies, samplingRate, numCycles)
+%
+% Inputs:
+%   phaseData: electrodes x timepoints x trials x frequencies dataset of
+%       phases
+%   timeInterval: two-value vector indicating start and end times of time
+%       interval in milliseconds
+%   eegTimes: vector from EEG.times
+%   frequencies: vector of frequencies in Hz
+%   samplingRate: sampling rate of the EEG data in Hz
+%   numCycles: number of cycles of data required for phase analysis; if a
+%       frequency is too slow to achieve numCycles within timeInterval, it
+%       will be excluded
+%
+% Outputs:
+%   phaseDataSample: electrodes x timepoints x trials x frequencies dataset
+%       of phases
+%   goodFreq: list of the frequencies included in the phaseDataSample
+%   badFreq: list of excluded frequencies
+%
+% Lindsay Vass
+% 15 September 2015
+
+timeInds  = find(eegTimes >= timeInterval(1) & eegTimes <= timeInterval(2));
+minFreq   = numCycles / (length(timeInds) / samplingRate);
+goodFreq  = frequencies(frequencies >= minFreq);
+badFreq   = frequencies(frequencies < minFreq);
+
+phaseDataSample = phaseData(:, timeInds, :, frequencies >= minFreq);
---
+++ Group Analysis/Phase/m/phaseLockingAnalysis.m
@@ -0,0 +1,123 @@
+function analyses = phaseLockingAnalysis(epochedEEGPath, goodChans, frequencies, waveletCycles, rayleighCycles, rayleighThresh, visualize)
+% phaseLockingAnalysis: test whether significant phase locking is observed
+% in the 500 ms after teleporter entry or exit
+% >> analyses = phaseLockingAnalysis(epochedEEGPath, goodChans, frequencies, waveletCycles, rayleighCycles, rayleighThresh, visualize)
+%
+% Inputs:
+%   epochedEEGPath: path to the epoched EEG dataset (.set file)
+%   frequencies: vector of frequencies in Hz
+%   goodChans: cell array of channel names for analysis
+%   waveletCycles: number of cycles to use for wavelet convolution when
+%       estimating phase (recommended = 6)
+%   rayleighCycles: number of cycles of data required for phase analysis;
+%       for example, 500 ms of data with rayleighCycles = 2 means that
+%       frequencies < 4 Hz will be excluded
+%   rayleighThresh: pvalue threshold for determining significance
+%       (conservative is good since we'll be doing many statistical tests)
+%   visualize: either 0 or 1; if 1, this will produce plots for each
+%       electrode for NT & FT showing a time-frequency plot of intertrial
+%       phase consistency; warning, takes a long time to produce plots
+%
+% Output:
+%   analyses: data structure containing the following fields
+%       AnalysisName: e.g., 'NT_Entry'
+%       InputData: matrix of input phase data
+%       TimeInterval: start and end times that were tested
+%       GoodFreq: frequencies that were included in the analysis
+%       BadFreq: frequencies that were excluded from analysis
+%       AnalyzedPhaseData: subset of InputData, restricted to valid time
+%           points and frequencies
+%       RayleighP: matrix of P values from the Rayleigh test
+%       SignifP: binary vector indicating whether RayleighP exceeded
+%           rayleighThresh
+%
+% Lindsay Vass
+% 15 September 2015
+
+% FOR TESTING PURPOSES
+% epochedEEGPath = '/Users/Lindsay/Documents/MATLAB/iEEG/Subjects/UCDMC15/Epoched Data/UCDMC15_TeleporterA_epoched_LAD_noSpikes_noWaves.set';
+% goodChans      = {'LAD3', 'LAD4', 'LAD5'};
+% frequencies    = logspace(log(1)/log(10),log(181)/log(10),31);
+% waveletCycles  = 6;
+% visualize      = 0; % produce plots of inter-trial phase clustering for each electrode? warning: takes a LONG time
+% rayleighCycles = 2; % number of cycles for which you must have significant rayleigh scores
+% rayleighThresh = 0.001; % p value must be lower than this to be deemed significant
+
+
+% calculate instantaneous phase at each time point for each frequency
+[ntPhase, ftPhase] = calcInstPhase(epochedEEGPath, frequencies, waveletCycles);
+
+% keep only good channels
+EEG = pop_loadset(epochedEEGPath);
+chanList = {EEG.chanlocs.labels};
+chanInds = nan(size(goodChans));
+for i = 1:length(goodChans)
+    chanInds(i) = find(strcmpi(goodChans{i}, chanList) == 1);
+end
+
+ntPhase = ntPhase(chanInds, :, :, :);
+ftPhase = ftPhase(chanInds, :, :, :);
+%% make analysis structure
+analyses = struct('AnalysisName', NaN, 'Channels', NaN, 'InputData', NaN, 'TimeInterval', NaN, 'GoodFreq', NaN, 'BadFreq', NaN, 'AnalyzedPhaseData', NaN, 'RayleighP', NaN, 'SignifP', NaN);
+
+analyses(1).AnalysisName = 'NT_Entry';
+analyses(1).InputData    = ntPhase;
+analyses(1).TimeInterval = [0 500];
+analyses(1).Channels     = goodChans;
+
+analyses(2).AnalysisName = 'NT_Exit';
+analyses(2).InputData    = ntPhase;
+analyses(2).TimeInterval = [1830 2330];
+analyses(2).Channels     = goodChans;
+
+analyses(3).AnalysisName = 'FT_Entry';
+analyses(3).InputData    = ftPhase;
+analyses(3).TimeInterval = [0 500];
+analyses(3).Channels     = goodChans;
+
+analyses(4).AnalysisName = 'FT_Exit';
+analyses(4).InputData    = ftPhase;
+analyses(4).TimeInterval = [2830 3330];
+analyses(4).Channels     = goodChans;
+
+%% run Rayleigh tests
+
+for thisAnalysis = 1:length(analyses)
+   [phaseDataSample, goodFreq, badFreq] = filterPhaseData(analyses(thisAnalysis).InputData, analyses(thisAnalysis).TimeInterval, EEG.times, frequencies, EEG.srate, rayleighCycles);
+   analyses(thisAnalysis).GoodFreq = goodFreq;
+   analyses(thisAnalysis).BadFreq  = badFreq;
+   analyses(thisAnalysis).AnalyzedPhaseData = phaseDataSample;
+   pData = nan(length(goodChans), size(phaseDataSample, 4), size(phaseDataSample, 2));
+   for thisElec = 1:size(phaseDataSample, 1)
+       for thisFreq = 1:size(phaseDataSample, 4)
+           [p, ~] = multRayleighTest(squeeze(phaseDataSample(thisElec, :, :, thisFreq))');
+           pData(thisElec, thisFreq, :) = p;
+       end
+   end
+   analyses(thisAnalysis).RayleighP = pData;
+   analyses(thisAnalysis).SignifP   = pData < rayleighThresh;
+end
+
+
+%% OPTIONAL: visualize the inter-trial phase locking
+if visualize == 1
+    ntITPC = nan(size(ntPhase, 1), size(ntPhase, 2), size(ntPhase, 4));
+    ftITPC = nan(size(ftPhase, 1), size(ftPhase, 2), size(ftPhase, 4));
+    for thisElec = 1:size(ntPhase, 1)
+        ntData = squeeze(ntPhase(thisElec, :, :, :));
+        ftData = squeeze(ftPhase(thisElec, :, :, :));
+        
+        ntITPC(thisElec, :, :) = squeeze(abs(mean(exp(1i*ntData), 2)));
+        ftITPC(thisElec, :, :) = squeeze(abs(mean(exp(1i*ftData), 2)));
+        
+        figure;
+        contourf(EEG.times, frequencies, squeeze(ntITPC(thisElec, :, :))', 40, 'linecolor', 'none');
+        set(gca, 'ytick', round(logspace(log10(frequencies(1)), log10(frequencies(end)), 10) * 100) / 100, 'yscale', 'log', 'clim', [0 0.6])
+        title(['NT Phase Clustering for ' EEG.chanlocs(thisElec).labels])
+        
+        figure;
+        contourf(EEG.times, frequencies, squeeze(ftITPC(thisElec, :, :))', 40, 'linecolor', 'none');
+        set(gca, 'ytick', round(logspace(log10(frequencies(1)), log10(frequencies(end)), 10) * 100) / 100, 'yscale', 'log', 'clim', [0 0.6])
+        title(['FT Phase Clustering for ' EEG.chanlocs(thisElec).labels])
+    end
+end
