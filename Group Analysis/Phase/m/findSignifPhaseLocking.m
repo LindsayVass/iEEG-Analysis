a8b1598958a38125371db602d1961eaaba4f2c41
LindsayVass <lindsay.k.morgan@gmail.com>
2015-09-17 10:48:07 -0700

    function and script to identify periods of significant phase locking

---
+++ Group Analysis/Phase/m/findSignifPhaseLocking.m
@@ -0,0 +1,49 @@
+% This script will examine the results of "runPhaseLockingAnalysis.m" to
+% determine whether there is evidence of significant sustained phase
+% locking. This is defined as a significant P value on the Rayleigh test
+% for all samples corresponding to 2 cycles of the test frequency, within
+% the 500 ms sampled interval.
+%
+% Lindsay Vass
+% 16 September 2015
+
+phaseResultsPath = '/Users/Lindsay/Documents/MATLAB/iEEG/Group Analysis/Phase/mat/phaseResults.mat';
+savePath         = '/Users/Lindsay/Documents/MATLAB/iEEG/Group Analysis/Phase/mat/signifPhaseResults.csv';
+
+%load(phaseResultsPath);
+samplingRate   = params.samplingRate;
+rayleighCycles = params.rayleighCycles;
+
+outputCell = cell(1, 7);
+outputCell(1, :) = {'Subject', 'Teleporter', 'Electrode', 'Analysis', 'Frequency', 'Onset', 'Offset'};
+for thisSubject = 1:length(phaseResults)
+    for thisTele = 1:length(phaseResults(thisSubject).teleporter)
+        for thisDepth = 1:length(phaseResults(thisSubject).teleporter(thisTele).depths)
+            for thisAnalysis = 1:length(phaseResults(thisSubject).teleporter(thisTele).depths(thisDepth).results)
+                for thisFreq = 1:length(phaseResults(thisSubject).teleporter(thisTele).depths(thisDepth).results(thisAnalysis).GoodFreq)
+                    
+                    frequency  = phaseResults(thisSubject).teleporter(thisTele).depths(thisDepth).results(thisAnalysis).GoodFreq(thisFreq);
+                    numContigP = round(rayleighCycles / frequency * samplingRate);
+                    
+                    for thisChan = 1:size(phaseResults(thisSubject).teleporter(thisTele).depths(thisDepth).results(thisAnalysis).SignifP, 1)
+                        pData = squeeze(phaseResults(thisSubject).teleporter(thisTele).depths(thisDepth).results(thisAnalysis).SignifP(thisChan, thisFreq, :));
+                        signifIntervals = identifySignifIntervals(pData, numContigP);
+                        if isempty(signifIntervals) == 0
+                            for i = 1:size(signifIntervals, 1)
+                                outputCell(end + 1, :) = {sessionInfo(thisSubject).subjectID, ...
+                                    phaseResults(thisSubject).teleporter(thisTele).name, ...
+                                    phaseResults(thisSubject).teleporter(thisTele).depths(thisDepth).chanList(thisChan), ...
+                                    phaseResults(thisSubject).teleporter(thisTele).depths(thisDepth).results(thisAnalysis).AnalysisName, ...
+                                    phaseResults(thisSubject).teleporter(thisTele).depths(thisDepth).results(thisAnalysis).GoodFreq(thisFreq), ...
+                                    phaseResults(thisSubject).teleporter(thisTele).depths(thisDepth).results(thisAnalysis).TimeMs(signifIntervals(i, 1)), ...
+                                    phaseResults(thisSubject).teleporter(thisTele).depths(thisDepth).results(thisAnalysis).TimeMs(signifIntervals(i, 2))};
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+
+cell2csv(savePath, outputCell)
---
+++ Group Analysis/Phase/m/identifySignifIntervals.m
@@ -0,0 +1,60 @@
+function signifIntervals = identifySignifIntervals(binaryVec, numContigVal)
+
+% identifySignifIntervals: determine whether there is a string of 1s in the
+% binaryVec that is at least numContigVal long
+% >> signifIntervals = identifySignifIntervals(binaryVec, numContigVal)
+%
+% Inputs:
+%   binaryVec: vector of binary values (1,0)
+%   numContigVal: number of contiguous 1s that must be observed for a
+%       significant effect
+%
+% Output:
+%   signifIntervals: 2-column matrix containing the onset (col1) and offset
+%       (col2) of each significant interval
+%
+% Lindsay Vass
+% 16 September 2015
+
+% Find the edges where powerBin changes from 0 --> 1 or 1 --> 0
+diffBinaryVec = diff(binaryVec);
+startInt = find(diffBinaryVec == 1) + 1;
+stopInt  = find(diffBinaryVec == -1);
+
+% Handle special edge cases
+if (isempty(startInt) && isempty(stopInt))
+    if(find(binaryVec) > 0)
+        % All values exceed powerThresh
+        signifIntervals = [1 length(binaryVec)]; 
+    else
+        % No values exceed powerThresh
+        signifIntervals = [];
+    end
+elseif (isempty(startInt))
+    % Starts on an episode and then stops
+    signifIntervals = [1 stopInt];
+elseif (isempty(stopInt))
+    % Starts an episode and continues until end of EEG
+    signifIntervals = [startInt length(binaryVec)];
+else
+    if (startInt(1) > stopInt(1))
+        % We started in an episode
+        startInt = [1 startInt];
+    end
+    if (stopInt(length(stopInt)) < startInt(length(startInt)))
+        % We ended on an episode
+        stopInt = [stopInt; length(binaryVec)];
+    end
+    signifIntervals = [startInt stopInt];
+end % special edge cases
+
+if (~isempty(signifIntervals)) 
+    % Find epochs that exceed the durationBinThresh
+    goodEpochs = find((signifIntervals(:, 2) - signifIntervals(:, 1) + 1) >= numContigVal);
+    
+    if (isempty(goodEpochs))
+        signifIntervals = [];
+    else
+        signifIntervals = signifIntervals(goodEpochs, :);
+    end
+end
